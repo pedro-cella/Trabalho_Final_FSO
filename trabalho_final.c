d29990012a83144f64c9f6f68b336f27c04a4510
Pedro Cella <pedrovscella@gmail.com>
2019-12-02 17:00:21 -0300

    Estabelecendo o que fazer no trabalho

---
+++ trabalho_final.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+
+//Achar o arquivo
+char DESTINO[] = argv[0];
+
+//Copiar o arquivo
+
+
+//Trabalhar no arquivo
+
+
+//Dividir de alguma forma em quatro partes(threads)
+
+
+//Compactar o arquivo
+
+
+//Fazer o tar do arquivo
+
+
+}
