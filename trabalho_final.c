67088299ef32c8fab95ab54973a32f300710ede7
Matheus-AM <mam.macacod@gmail.com>
2019-12-06 15:31:58 -0300

    funcionando sem thread

--- trabalho_final.c
+++ trabalho_final.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define PATH_MAX 4096
 
@@ -33,69 +34,62 @@ struct Pool_of_consumers{
     char filename[PATH_MAX];
 }; typedef struct Pool_of_consumers Pool;
 
-struct ArvoreBinaria
-{
-    int valor;
-    struct ArvoreBinaria *esquerda;
-    struct ArvoreBinaria *direita;
-};
-typedef struct ArvoreBinaria Arvore;
-
-int percorrerDiretorio(char *);
-
-void loadTreeFromFile(Arvore **, char*);
-int isFull(Arvore **, int, int, int);
-int searchValue(Arvore **, int);
-int getHeight(Arvore **, int, int);
-//int removeValue(Arvore **, int);
-void printInOrder(Arvore **);
-void printPreOrder(Arvore **);
-void printPostOrder(Arvore **);
-void inserirValor(Arvore **, int);
-Arvore *criarArvoreVazia();
-void freeArvore(Arvore **);
-
+int bz2Recursivo(char *);
+int removerRecursivo(char *);
+int copiarArquivo(char *, char *);
+int copiarRecursivo(char *, char *);
+//char *completarCaminho(char *, char *);
 
 
 int main(int argc, char *argv[]){
 
-    char *dir = argv[1];
-    char *DESTINO = argv[2];
-
-    char *dirAtual = (char *) malloc(4096*sizeof(char));
-    dirAtual = dir;
-
+    char dir[PATH_MAX];
+    strcpy(dir,argv[1]);
+    char *DESTINO = strtok(argv[2], ".");
     char buffer[PATH_MAX];
 
-
-    //Entrar no diretorio
-    if(chdir(dir))  perror("nao encontrou o diretorio.\n");
-
-    //percorrer o diretorio recursivamente
-
-    percorrerDiretorio(dir);
-    //Copiar os arquivos recursivamente
-
-    //Chamar o shell para fazer bzip2 (apenas em arquivos) em 4 threads (recursivamente)
-
-    sprintf(buffer, "bzip2 %s", DESTINO);
-    if(system(buffer)) perror("nao foi possivel fazer o bzip2 do arquivo.");
-
-
-    //Chamar o shell para fazer o tar
-//    if(chdir("..")) perror("nao encontrou o diretorio.\n");
-    sprintf(buffer, "tar cf %s.tar %s", DESTINO, DESTINO);
-    if(system(buffer)){
-        perror("nao foi possivel fazer o tar do diretorio.");
+    if (strcmp(strtok(argv[1], "/"), "home")) {
+        char dirAtual[PATH_MAX];
+        if (getcwd(dirAtual, sizeof(dirAtual)) == NULL) {
+            perror("getcwd() error");
+            exit(-1);
+        }
+        strcat(dirAtual, "/");
+        strcat(dirAtual, dir);
+        strcpy(dir, dirAtual);
     }
 
+
+    //Copiar recursivamente
+    strcat(DESTINO, ".bz2");
+    mkdir(DESTINO, 0700);
+    if(chdir(DESTINO))  perror("nao encontrou o diretorio.\n");
+    char novo[PATH_MAX];
+    if (getcwd(novo, sizeof(novo)) == NULL) {
+        perror("getcwd() error");
+        exit(-1);
+    }
+    copiarRecursivo(dir, novo);
+    if(chdir(novo)) perror("erro no chdir()");
+
+//    //Chamar o shell para fazer bzip2 (apenas em arquivos) em 4 threads (recursivamente)
+    // threads
+//    bz2Recursivo(novo);
+//    if(chdir(novo)) perror("erro no chdir()");
+
+    //Chamar o shell para fazer o tar
+    if(chdir("..")) perror("erro no chdir()");
+    sprintf(buffer, "tar cf %s.tar %s", DESTINO, DESTINO);
+//    if(popen(buffer, "r") == NULL) perror("nao foi possivel fazer o tar do diretorio.");
+    system(buffer);
+    if(chdir(novo)) perror("erro no chdir()");
+
+
     //Excluir a copia A TERMINAR
-    char path[PATH_MAX];
-    strcpy(path, dir);
-    strcat(path, "/");
-    strcat(path, DESTINO);
-    strcat(path, ".tar");
-    if(unlink(path)) perror("nao foi possivel remover o arquivo.");
+    sleep(1);
+    if(removerRecursivo(novo)) perror("nao foi possivel remover o arquivo.");
+    chdir("..");
+    rmdir(DESTINO);
     //A TERMINAR
 
 
@@ -103,7 +97,7 @@ int main(int argc, char *argv[]){
 }
 
 
-int percorrerDiretorio(char *dirAtual){
+int bz2Recursivo(char *dirAtual){
 
 
     Diretorio *nameList; //se o d_type == 4, entao ele e um diretorio.
@@ -112,147 +106,119 @@ int percorrerDiretorio(char *dirAtual){
     numeroDeArquivos = scandir(dirAtual, &nameList, 0, alphasort);
     if(numeroDeArquivos < 0) perror("erro ao visualizar os arquivos.");
     for (int i = 0; i < numeroDeArquivos; ++i) {
-        printf("%d , %s\n", nameList[i]->d_type, nameList[i]->d_name);
+//        printf("%d , %s\n", nameList[i]->d_type, nameList[i]->d_name);
+
         if(nameList[i]->d_type == 4 && strcmp(nameList[i]->d_name, ".") && strcmp(nameList[i]->d_name, "..")) {
             char dirAux[PATH_MAX];
             strcpy(dirAux, dirAtual);
             strcat(dirAux, "/");
             strcat(dirAux, nameList[i]->d_name);
-            percorrerDiretorio(dirAux);
+            bz2Recursivo(dirAux);
+        }else if(strcmp(nameList[i]->d_name, ".") && strcmp(nameList[i]->d_name, "..")) {
+            char buffer[PATH_MAX];
+            chdir(dirAtual);
+            sprintf(buffer, "bzip2 %s", nameList[i]->d_name);
+            if (popen(buffer, "r") == NULL) perror("nao foi possivel fazer o bzip2 do arquivo.");
         }
-        free(nameList[numeroDeArquivos]);
     }
-//    free(nameList);
     return 0;
 }
 
-void inserirValor(Arvore **atual, int valor){
-    if(*atual == NULL){
-        *atual = (Arvore *) malloc(sizeof(Arvore));
-        (*atual)->valor = valor;
-        (*atual)->esquerda = NULL;
-        (*atual)->direita = NULL;
-    }else {
-        if(valor <= (*atual)->valor)
-            inserirValor(&(*atual)->esquerda, valor);
-        else
-            inserirValor(&(*atual)->direita, valor);
-    }
-}
+int removerRecursivo(char *dirAtual){
 
-Arvore *criarArvoreVazia(){
-    return NULL;
-}
+    Diretorio *nameList; //se o d_type == 4, entao ele e um diretorio.
+    int numeroDeArquivos;
 
-int searchValue(Arvore **atual, int busca){
-    if(*atual == NULL){
-        printf("Valor nao encontrado.\n");
-        return 0;
-    }
-    if(busca == (*atual)->valor){
-        return busca;
-    }else {
-        if(busca < (*atual)->valor)
-            return searchValue(&(*atual)->esquerda, busca);
-        else
-            return searchValue(&(*atual)->direita, busca);
-    }
-}
+    numeroDeArquivos = scandir(dirAtual, &nameList, 0, alphasort);
+    if(numeroDeArquivos < 0) perror("erro ao visualizar os arquivos.");
+    for (int i = 0; i < numeroDeArquivos; ++i) {
 
-int isFull(Arvore **atual, int profundidade, int altura, int validacao){
-    if((*atual)->esquerda != NULL ){
-        profundidade++;
-        validacao = isFull(&(*atual)->esquerda, profundidade, altura, validacao);
-    }
-    if((*atual)->direita != NULL){
-        profundidade++;
-        validacao = isFull(&(*atual)->direita, profundidade, altura, validacao);
-    }
+        char dirAux[PATH_MAX];
+        strcpy(dirAux, dirAtual);
+        strcat(dirAux, "/");
+        strcat(dirAux, nameList[i]->d_name);
 
-    if(profundidade != altura)
-        return 1;
-    else
-        return validacao;
-}
-
-void printInOrder(Arvore **atual){
-    if((*atual)->esquerda != NULL ){
-        printInOrder(&(*atual)->esquerda);
-    }
-    printf("%d\t", (*atual)->valor);
-    if((*atual)->direita != NULL){
-        printInOrder(&(*atual)->direita);
-    }
-}
-
-void printPreOrder(Arvore **atual){
-    printf("%d\t", (*atual)->valor);
-    if((*atual)->esquerda != NULL ){
-        printPreOrder(&(*atual)->esquerda);
-    }
-    if((*atual)->direita != NULL){
-        printPreOrder(&(*atual)->direita);
-    }
-}
-
-void printPostOrder(Arvore **atual){
-    if((*atual)->esquerda != NULL ){
-        printPostOrder(&(*atual)->esquerda);
-    }
-    if((*atual)->direita != NULL){
-        printPostOrder(&(*atual)->direita);
-    }
-    printf("%d\t", (*atual)->valor);
-}
-
-/*
-int removeValue(Arvore **atual, int valor){
-    if(*atual == NULL){
-//        printf("Valor nao encontrado.\n");
-        return 0;
-    }
-    if(valor == (*atual)->valor){
-        free(*atual);
-        return 1;
-    }else {
-        int status;
-        if (valor < (*atual)->valor){
-            if (removeValue(&(*atual)->esquerda, valor))
-                (*atual)->esquerda = NULL;
-        }else {
-            if (removeValue(&(*atual)->direita, valor))
-                (*atual)->direita = NULL;
+//        printf("%d %s\n", nameList[i]->d_type, dirAux);
+        if(nameList[i]->d_type == 4 && strcmp(nameList[i]->d_name, ".") && strcmp(nameList[i]->d_name, "..")) { //se for diretorio
+            removerRecursivo(dirAux);
+            if(remove(dirAux)) perror("nao foi possivel remover o diretorio.");
+        } else if(strcmp(nameList[i]->d_name, ".") && strcmp(nameList[i]->d_name, "..")) { //se for arquivo
+            if(remove(dirAux)) perror("nao foi possivel remover o arquivo.");
         }
-        return 0;
     }
-}
-*/
-
-
-int getHeight(Arvore **atual, int alturaE, int alturaD){
-    if((*atual)->esquerda != NULL ){
-        alturaE++;
-        alturaE = getHeight(&(*atual)->esquerda, alturaE, alturaE);
-    }
-    if((*atual)->direita != NULL){
-        alturaD++;
-        alturaD = getHeight(&(*atual)->direita, alturaD, alturaD);
-    }
-
-    if(alturaE >= alturaD)
-        return alturaE;
-    else
-        return alturaD;
+    return 0;
 }
 
+int copiarArquivo(char *arquivo, char *destino){
+    FILE *fptr1, *fptr2;
+    char novo[PATH_MAX];
 
-void freeArvore(Arvore **atual){
-    if((*atual)->esquerda != NULL ){
-        freeArvore(&(*atual)->esquerda);
+    // Open one file for reading
+    fptr1 = fopen(arquivo, "r");
+    if (fptr1 == NULL)
+    {
+        printf("Cannot open file %s \n", arquivo);
+        return -1;
     }
-    if((*atual)->direita != NULL){
-        freeArvore(&(*atual)->direita);
+
+    fptr2 = fopen(destino, "w");
+    if (fptr2 == NULL)
+    {
+        printf("Cannot open file %s \n", destino);
+        return -1;
     }
-    free(*atual);
+
+    // Read contents from file
+    signed c;
+    c = fgetc(fptr1);
+    while (c != EOF)
+    {
+        fputc(c, fptr2);
+        c = fgetc(fptr1);
+    }
+
+//    printf("\nContents copied to %s\n", destino);
+
+    fclose(fptr1);
+    fclose(fptr2);
+
+    return 0;
 }
+
+int copiarRecursivo(char *dirAtual, char *novo){
+
+    Diretorio *nameList; //se o d_type == 4, entao ele e um diretorio.
+    int numeroDeArquivos;
+
+    numeroDeArquivos = scandir(dirAtual, &nameList, 0, alphasort);
+    if(numeroDeArquivos < 0) perror("erro ao visualizar os arquivos.");
+    for (int i = 0; i < numeroDeArquivos; ++i) {
+
+        char dirAux[PATH_MAX];
+        strcpy(dirAux, dirAtual);
+        strcat(dirAux, "/");
+        strcat(dirAux, nameList[i]->d_name);
+        char dirNovo[PATH_MAX];
+        strcpy(dirNovo, novo);
+        strcat(dirNovo, "/");
+        strcat(dirNovo, nameList[i]->d_name);
+
+//        printf("%d %s\n", nameList[i]->d_type, dirAux);
+        if(nameList[i]->d_type == 4 && strcmp(nameList[i]->d_name, ".") && strcmp(nameList[i]->d_name, "..")) { //se for diretorio
+            if(mkdir(nameList[i]->d_name, 0700)) perror("erro no mkdir()");
+            if(chdir(dirNovo)) perror("erro no chdir()");
+            copiarRecursivo(dirAux, dirNovo);
+            if(chdir("..")) perror("erro no chdir()");
+        } else if(strcmp(nameList[i]->d_name, ".") && strcmp(nameList[i]->d_name, "..")) { //se for arquivo
+            copiarArquivo(dirAux, dirNovo);
+            char buffer[PATH_MAX];
+            sprintf(buffer, "bzip2 %s", nameList[i]->d_name);
+//            if (popen(buffer, "r") == NULL) perror("nao foi possivel fazer o bzip2 do arquivo.");
+            system(buffer);
+        }
+    }
+
+    return 0;
+}
+
 
